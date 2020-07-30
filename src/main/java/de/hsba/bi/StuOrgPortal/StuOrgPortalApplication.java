9931d7f288a9c16723fc050455fe90874c50de4f
Jonas.Noehr <jonasnoehr2908@gmail.com>
2020-07-30 09:44:57 +0200

    Initial Project

---
+++ src/main/java/de/hsba/bi/StuOrgPortal/StuOrgPortalApplication.java
@@ -0,0 +1,14 @@
+package de.hsba.bi.StuOrgPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuOrgPortalApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(StuOrgPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hsba/bi/StuOrgPortal/StuOrgPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsba.bi.StuOrgPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StuOrgPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
