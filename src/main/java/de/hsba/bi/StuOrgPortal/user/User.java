b721b57c3ae514aa86f17357d687bc15153547e0
Marie Heymann <mheymann@backfactory.de>
2020-08-11 12:08:48 +0200

    .

---
+++ src/main/java/de/hsba/bi/StuOrgPortal/user/User.java
@@ -0,0 +1,11 @@
+package de.hsba.bi.StuOrgPortal.user;
+
+public class User {
+    public static void admin(String[] args) {
+
+        String userName = "Admin";
+        String password = "Passwort";
+        
+    }
+
+}
---
+++ src/main/java/de/hsba/bi/StuOrgPortal/user/UserService.java
@@ -0,0 +1,11 @@
+package de.hsba.bi.StuOrgPortal.user;
+
+/**
+ * In dieser Klasse soll das Verhalten der einzelenen User-Objekte definiert werden
+ */
+
+public class UserService {
+
+
+
+}
