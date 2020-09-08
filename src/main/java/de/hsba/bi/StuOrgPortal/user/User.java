4b1286edd27f7da0bcd427f9a294758403932dd3
jonas <jonasnoehr2908@gmail.com>
2020-09-08 08:49:11 +0200

    User Changes, anmelden abmelden

---
+++ src/main/java/de/hsba/bi/StuOrgPortal/user/User.java
@@ -0,0 +1,68 @@
+package de.hsba.bi.StuOrgPortal.user;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User implements Comparable<User> {
+
+    public static String STUDENT_ROLE = "STUDENT";
+    public static String LECTURER_ROLE = "LECTURER";
+    public static String ADMIN_ROLE = "ADMIN";
+
+    public static String getCurrentUsername() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            return ((UserDetails) principal).getUsername();
+        }
+        return null;
+    }
+
+    @Id
+    @GeneratedValue
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @Basic(optional = false)
+    private String name;
+
+    @Basic(optional = false)
+    private String password;
+
+    @Setter
+    private String role;
+
+    /*public User(String name) {
+        this.name = name;
+    }
+
+     */
+
+    public User(String name, String password, String role) {
+        this.name = name;
+        this.password = password;
+        this.role = role;
+    }
+
+    @Override
+    public int compareTo(User other) {
+        return this.name.compareTo(other.name);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
